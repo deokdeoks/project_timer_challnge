2988dad6a4ec9a4ecb5c201fce0cdb9b869acd9a
duckyear <kdn2869@naver.com>
2025-06-03 17:47:12 +0900

    프로젝트 내용 커밋

---
+++ src/components/TimerChallenger.jsx
@@ -0,0 +1,43 @@
+import {useState, useRef} from "react";
+import ResultModal from "./ResultModal.jsx";
+
+export default function TimerChallenger({title,targetTime}){
+    const timer = useRef();
+    const  dialog = useRef();
+    const  [timerStarted,setTimerStarted  ]= useState(false);
+    const  [timerExpired,setTimerExpired  ]= useState(false);
+
+    function handleStart(){
+      timer.current = setTimeout(() => {
+            setTimerExpired(true);
+            dialog.current.showModal();
+        }, targetTime * 1000);
+        setTimerStarted(true);
+    }
+
+    function handleStop(){
+        clearTimeout(timer.current);
+    }
+
+    return(
+    <>
+    <ResultModal ref={dialog} targetTime={targetTime} result="lost"/>
+    <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>you lost!</p>}
+        <p className="challenge-time">
+            {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+            <button onClick={timerStarted ? handleStop : handleStart}>
+                {timerStarted ? 'Stop' : 'Star'} Challenge
+            </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>
+            Time is running / Timer inactive
+        </p>
+    </section>
+    </>
+     );
+}
+
